3f253597aad3d5c3c355a67d0fdfc60bb6333d92
Matiullah Karimi <matiullah.karimi@gmail.com>
2020-12-02 09:29:28 +0430

    Refactor databse connection code

--- database.js
+++ database.js
@@ -1,11 +1,12 @@
 const mongoose = require('mongoose');
 
-module.exports = function() {
-  mongoose.connect('mongodb://localhost/residential_passport', { useNewUrlParser: true })
-    .then(() => {
-      console.log('Successfully connected to mongodb....');
-    })
-    .catch(error => {
-      console.error('Failed to connect to mongodb database: ', error);
-    });
+module.exports = async function() {
+    try {
+        await mongoose.connect('mongodb://localhost/residential_passport', 
+            { useNewUrlParser: true, useUnifiedTopology: true });
+        
+        console.log('Connected to MongoDB Database...')
+    } catch (error) {
+        console.error('Failed to connect to mongodb database: ', error);
+    }
 }
