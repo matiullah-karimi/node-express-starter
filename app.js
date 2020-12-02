b51c8d8e27c7bdaf5700daf9c399c82090180868
Matiullah Karimi <matiullah.karimi@gmail.com>
2020-12-02 11:06:36 +0430

    Add api prefix for routes

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(passport.initialize());
-app.use('/', indexRouter);
+app.use('/api', indexRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
