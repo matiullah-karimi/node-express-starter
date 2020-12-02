fdfacdfe0751e4ed71e4377512ee71392fdba181
Matiullah Karimi <matiullah.karimi@gmail.com>
2020-12-02 13:09:53 +0430

    Add login validator

---
+++ middleware/validators/login.validator.js
@@ -0,0 +1,23 @@
+const Joi = require('joi');
+const { toCustomValidationError } = require('../../utils/custom-validation-errors.util');
+const { sendJsonResponse } = require('../../utils/response.util');
+
+const schema = Joi.object({
+    email: Joi.string().email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }).required(),
+    password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')).required()
+});
+
+module.exports = (req, res, next) => {
+    try {
+        const { error } = schema.validate(req.body, { abortEarly: false });
+
+        if (error) {
+            return sendJsonResponse(res, 200, toCustomValidationError(error));
+        }
+
+        next();
+    }
+    catch (err) { 
+        sendJsonResponse(res, 500, err);
+    }
+};
--- routes/index.route.js
+++ routes/index.route.js
@@ -3,13 +3,14 @@ const router = express.Router();
 const userRouter = require('./users.route');
 const passport = require('passport');
 const loginController = require('../controllers/login.controller');
+const loginValidator = require('../middleware/validators/login.validator');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.post('/login', loginController.login)
+router.post('/login', loginValidator, loginController.login)
 router.use('/users', passport.authenticate('jwt', { session: false }), userRouter);
 
 module.exports = router;
