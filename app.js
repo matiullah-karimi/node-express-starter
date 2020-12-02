a94e007bdd73da0623c38b09a291bac22c0106c0
Matiullah Karimi <matiullah.karimi@gmail.com>
2020-12-02 10:40:16 +0430

    Add login route

--- app.js
+++ app.js
@@ -3,9 +3,8 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-
+const passport = require('passport');
 const indexRouter = require('./routes/index.route');
-
 const app = express();
 
 // view engine setup
@@ -17,7 +16,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(passport.initialize());
 app.use('/', indexRouter);
 
 // catch 404 and forward to error handler
--- routes/index.route.js
+++ routes/index.route.js
@@ -2,12 +2,14 @@ const express = require('express');
 const router = express.Router();
 const userRouter = require('./users.route');
 const passport = require('passport');
+const loginController = require('../controllers/login.controller');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.post('/login', loginController.login)
 router.use('/users', passport.authenticate('jwt'), userRouter);
 
 module.exports = router;
