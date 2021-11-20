ecaf8df0e163aaadd53571552f2f9e1302c2fc49
Matiullah Karimi <matiullah.karimi@gmail.com>
2021-11-20 08:30:26 +0400

    Return json instead of rendering page while error

--- app.js
+++ app.js
@@ -32,7 +32,10 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.json({
+    status: err.status || 500,
+    msg: err.message
+  }).send();
 });
 
 require('dotenv').config();
