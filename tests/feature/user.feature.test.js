a975baffa2e53d8e54c04b27c287e162fc662797
Matiullah Karimi <matiullah.karimi@gmail.com>
2021-11-22 12:04:49 +0400

    Add tests for user creation feature

--- tests/feature/user.feature.test.js
+++ tests/feature/user.feature.test.js
@@ -38,7 +38,67 @@ describe('POST /api/users', () => {
                 expect(response.body.password).toBeUndefined();
             });
 
-            const user = await User.find({email: userData.email});
+            const user = await User.findOne({email: userData.email});
             expect(user).toBeDefined();    
+    });
+
+    it('should return 401 if user is not authenticated', async () => {
+        let userData = {
+            name: 'Test User',
+            role: roles.ADMIN,
+            email: 'test@starter.com',
+            phone: '09999999999',
+            password: 'secret',
+        };
+
+        await request(app).post("/api/users")
+            .send(userData)
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .expect(401);
+
+            const user = await User.findOne({email: userData.email});
+            expect(user).toBeNull();    
+    });
+
+    it('should return 403 if user does not have the Admin role', async () => {
+        let userData = {
+            name: 'Test User',
+            role: roles.REVIEWER,
+            email: 'test@starter.com',
+            phone: '09999999999',
+            password: 'secret',
+        };
+
+        const reviewer = await UserFactory.create({role: roles.REVIEWER});
+
+        await request(app).post("/api/users")
+            .send(userData)
+            .set('Accept', 'application/json')
+            .set('Authorization', 'Bearer ' + reviewer.generateAuthToken())
+            .expect('Content-Type', /json/)
+            .expect(403);
+
+            const user = await User.findOne({email: userData.email});
+            expect(user).toBeNull();    
+    });
+
+    it('should return 422 if user does not have the Admin role', async () => {
+        let userData = {};
+
+        const adminUser = await UserFactory.create({role: roles.ADMIN});
+
+        await request(app).post("/api/users")
+            .send(userData)
+            .set('Accept', 'application/json')
+            .set('Authorization', 'Bearer ' + adminUser.generateAuthToken())
+            .expect('Content-Type', /json/)
+            .expect(422)
+            .then(response => {
+                expect(Object.keys(response.body)).toEqual(['name', 'email', 'phone', 'role', 'password'])
+            });
+
+            const count = await User.countDocuments({});
+            expect(count).toBe(1);    
     });
 });
